1460a78af3e5be747ab904dfea1c7730493c48fd
Chase Whitfield <chase.whitfield@itential.com>
2025-04-01 14:36:28 -0700

    add sha256

--- Formula/torero.rb
+++ Formula/torero.rb
@@ -7,22 +7,22 @@ class Torero < Formula
 
   resource "mac_amd" do
     url "https://download.torero.dev/torero-v1.3.0-darwin-amd64.tar.gz"
-    sha2 "d6001379610b0c5febbe86cc6bf17ecca13b98be7d4e08d684eea0e3aaea44d118d11caec652aa23fa8790a1018f522d91b9295af855a686f289e9b2fc684581"
+    sha256 "654758e5dc1b799feaee6229d3378abfa8ff7ff7df72e9c12215776c9633816a"
   end
 
   resource "mac_arm" do
     url "https://download.torero.dev/torero-v1.3.0-darwin-arm64.tar.gz"
-    sha2 "99c976657c7f176273b4732922836a2f3cc36bdf96e5eab01162c8e9cdd0c74bb5188b0f5b99efd467e2db525d32d61d1beed5ca488c06105f97e9947603a9fb"
+    sha256 "20c312ef4016d37f701c3ce2a5b0b37ebb6304f7020dd567f25c368beac0e25f"
   end
 
   resource "linux_amd" do
     url "https://download.torero.dev/torero-v1.3.0-linux-amd64.tar.gz"
-    sha2 "2e91bfd06e232d80493717896d22b8094a136be7dbabdcc4c6ccd489e187a08eae6415d9d40baa199943c9f81a2515e40e8928515e8f10c58995e004c12706c6"
+    sha256 "ef99444ea08455b2eb7e16bc32f7d8193b9d1247f8fb08dec7ac22d63c6286d7"
   end
 
   resource "linux_arm" do
     url "https://download.torero.dev/torero-v1.3.0-linux-arm64.tar.gz"
-    sha2 "5cde3613aff02df6854e1b48ef614000111f11d082b2d779bc7d2b532ca288cfba759ae01e8436e8ae1bf7b8afa1c5d805eebacd221eac51ef314d3eddb9d2ca"
+    sha256 "de9b3f239d9f1adee2711906e18cd0fdec2f69f5da3513f443a71a956d1cb4a8"
   end
 
   def install
