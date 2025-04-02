9b63dd4e46781da4aeb55fe717d3603f74e63c44
Chase Whitfield <chase.whitfield@itential.com>
2025-04-02 16:47:56 -0700

    updates

--- Formula/torero.rb
+++ Formula/torero.rb
@@ -8,47 +8,39 @@ class Torero < Formula
 
   if OS.mac?
     if Hardware::CPU.intel?
-      resource "torero_resource" do
-        url "https://download.torero.dev/torero-v1.3.0-darwin-amd64.tar.gz"
-        sha256 "654758e5dc1b799feaee6229d3378abfa8ff7ff7df72e9c12215776c9633816a" # darwin-amd64
-      end
+      url "https://download.torero.dev/torero-v1.3.0-darwin-amd64.tar.gz"
+      sha256 "654758e5dc1b799feaee6229d3378abfa8ff7ff7df72e9c12215776c9633816a" # darwin-amd64
+    elsif Hardware::CPU.arm?
+      url "https://download.torero.dev/torero-v1.3.0-darwin-arm64.tar.gz"
+      sha256 "20c312ef4016d37f701c3ce2a5b0b37ebb6304f7020dd567f25c368beac0e25f" # darwin-arm64
     end
-
-    if Hardware::CPU.arm?
-      resource "torero_resource" do
-        url "https://download.torero.dev/torero-v1.3.0-darwin-arm64.tar.gz"
-        sha256 "20c312ef4016d37f701c3ce2a5b0b37ebb6304f7020dd567f25c368beac0e25f" # darwin-arm64
-      end
-    end
-  end
-
-  if OS.linux?
+  elsif OS.linux?
     if Hardware::CPU.intel?
-      resource "torero_resource" do
-        url "https://download.torero.dev/torero-v1.3.0-linux-amd64.tar.gz"
-        sha256 "ef99444ea08455b2eb7e16bc32f7d8193b9d1247f8fb08dec7ac22d63c6286d7" # linux-amd64
-      end
-    end
-
-    if Hardware::CPU.arm?
-      resource "torero_resource" do
-        url "https://download.torero.dev/torero-v1.3.0-linux-arm64.tar.gz"
-        sha256 "de9b3f239d9f1adee2711906e18cd0fdec2f69f5da3513f443a71a956d1cb4a8" # linux-arm64
-      end
+      url "https://download.torero.dev/torero-v1.3.0-linux-amd64.tar.gz"
+      sha256 "ef99444ea08455b2eb7e16bc32f7d8193b9d1247f8fb08dec7ac22d63c6286d7" # linux-amd64
+    elsif Hardware::CPU.arm?
+      url "https://download.torero.dev/torero-v1.3.0-linux-arm64.tar.gz"
+      sha256 "de9b3f239d9f1adee2711906e18cd0fdec2f69f5da3513f443a71a956d1cb4a8" # linux-arm64
     end
   end
 
   def install
-    resource("torero_resource").stage { bin.install "./torero" => "torero" }
+    odie "Unsupported architecture" if url == "file:///dev/null"?
+
+    ohai "------- Installing Torero -------"
+
+    bin.install "./torero" => "torero"
   end
 
   def post_install
-    puts "------ RUNNING POST INSTALL ------"
+    ohai "------- Starting Torero -------"
+
     system bin/"torero"
   end
 
   test do
-    puts "------ RUNNING TESTS ------"
+    ohai "------- Testing Torero Install -------"
+
     assert_path_exists bin/"torero"
   end
 end
