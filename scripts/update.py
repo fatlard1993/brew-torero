d0a1285e779abf63cecee777f6bae56ac00b6027
Chase Whitfield <chase.whitfield@itential.com>
2025-04-02 15:21:15 -0700

    add info prints

--- scripts/update.py
+++ scripts/update.py
@@ -86,6 +86,9 @@ for architecture in architecture:
     architectureMap[architecture]["url"] = url
     architectureMap[architecture]["sha256"] = get_sha256_from_url(url)
 
+print(f"Targeting {gitRemote}")
+print(f"Registering version {version}")
+
 try:
     clone_repo()
 except Exception as e:
