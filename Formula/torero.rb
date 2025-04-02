386bc6bd8b08f132e1f540e08fa4a457797d54a0
Chase Whitfield <chase.whitfield@itential.com>
2025-04-02 12:35:05 -0700

    wed co-working progress

--- Formula/torero.rb
+++ Formula/torero.rb
@@ -6,47 +6,49 @@ class Torero < Formula
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MIT"
 
-  resource "mac_intel" do
-    url "https://download.torero.dev/torero-v1.3.0-darwin-amd64.tar.gz"
-    sha256 "654758e5dc1b799feaee6229d3378abfa8ff7ff7df72e9c12215776c9633816a"
+  if OS.mac?
+    if Hardware::CPU.intel?
+      resource "torero_resource" do
+        url "https://download.torero.dev/torero-v1.3.0-darwin-amd64.tar.gz"
+        sha256 "654758e5dc1b799feaee6229d3378abfa8ff7ff7df72e9c12215776c9633816a"
+      end
+    end
+
+    if Hardware::CPU.arm?
+      resource "torero_resource" do
+        url "https://download.torero.dev/torero-v1.3.0-darwin-arm64.tar.gz"
+        sha256 "20c312ef4016d37f701c3ce2a5b0b37ebb6304f7020dd567f25c368beac0e25f"
+      end
+    end
   end
 
-  resource "mac_arm" do
-    url "https://download.torero.dev/torero-v1.3.0-darwin-arm64.tar.gz"
-    sha256 "20c312ef4016d37f701c3ce2a5b0b37ebb6304f7020dd567f25c368beac0e25f"
-  end
+  if OS.linux?
+    if Hardware::CPU.intel?
+      resource "torero_resource" do
+        url "https://download.torero.dev/torero-v1.3.0-linux-amd64.tar.gz"
+        sha256 "ef99444ea08455b2eb7e16bc32f7d8193b9d1247f8fb08dec7ac22d63c6286d7"
+      end
+    end
 
-  resource "linux_intel" do
-    url "https://download.torero.dev/torero-v1.3.0-linux-amd64.tar.gz"
-    sha256 "ef99444ea08455b2eb7e16bc32f7d8193b9d1247f8fb08dec7ac22d63c6286d7"
-  end
-
-  resource "linux_arm" do
-    url "https://download.torero.dev/torero-v1.3.0-linux-arm64.tar.gz"
-    sha256 "de9b3f239d9f1adee2711906e18cd0fdec2f69f5da3513f443a71a956d1cb4a8"
+    if Hardware::CPU.arm?
+      resource "torero_resource" do
+        url "https://download.torero.dev/torero-v1.3.0-linux-arm64.tar.gz"
+        sha256 "de9b3f239d9f1adee2711906e18cd0fdec2f69f5da3513f443a71a956d1cb4a8"
+      end
+    end
   end
 
   def install
-    if OS.mac?
-      resource("mac_intel").stage { bin.install "./torero" => "torero" } if Hardware::CPU.intel?
-
-      resource("mac_arm").stage { bin.install "./torero" => "torero" } if Hardware::CPU.arm?
-    end
-
-    if OS.linux?
-      resource("linux_intel").stage { bin.install "./torero" => "torero" } if Hardware::CPU.intel?
-
-      resource("linux_arm").stage { bin.install "./torero" => "torero" } if Hardware::CPU.arm?
-    end
+    resource("torero_resource").stage { bin.install "./torero" => "torero" }
   end
 
   def post_install
-    system bin/"torero", "--version"
+    puts "------ RUNNING POST INSTALL ------"
+    system bin/"torero"
   end
 
   test do
+    puts "------ RUNNING TESTS ------"
     assert_path_exists bin/"torero"
-
-    system bin/"torero", "--version"
   end
 end
